a152a8770f2275c3324ed0ece2a1e5468c42282c
Elias Sells <esell002@ucr.edu>
2019-02-28 14:21:12 -0800

    Initial commit

---
+++ esell002_SpaceInvaders/esell002_SpaceInvaders/main.c
@@ -0,0 +1,20 @@
+/* Elias Sells esell002@ucr.edu
+* Lab Section: 23
+* Assignment: Custom Lab Project: Space Invaders
+* Exercise Description: [optional - include for your own benefit]
+*
+* I acknowledge all content contained herein, excluding template or example
+* code, is my own original work.
+*/
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	/* Replace with your application code */
+	while (1)
+	{
+	}
+}
+

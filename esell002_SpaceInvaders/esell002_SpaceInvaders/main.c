4d0bd100c9c02b9146a6e349b8554c7958e75d82
Elias Sells <esell002@ucr.edu>
2019-03-07 15:32:29 -0800

    Added some more functionality

--- esell002_SpaceInvaders/esell002_SpaceInvaders/main.c
+++ esell002_SpaceInvaders/esell002_SpaceInvaders/main.c
@@ -9,19 +9,62 @@
 
 #include <avr/io.h>
 #include "nokia5110.c"
+#include "timer.h"
 
 
 int main(void)
-{
+{	
+	TimerSet(100);
+	TimerOn();
+	DDRB = 0x00; PORTB = 0xFF;
+	unsigned char XPos = 38;
+	unsigned char YPos = 42;
+	unsigned char shootLaser = 0;
+	unsigned char LaserXPos = 0;
+	unsigned char LaserYPosStart = 36;
+	unsigned char LaserYPos = 0;
     nokia_lcd_init();
     nokia_lcd_clear();
     nokia_lcd_write_string("IT'S WORKING!",1);
-    nokia_lcd_set_cursor(0, 10);
-    nokia_lcd_write_string("Nice!", 3);
+    //nokia_lcd_set_cursor(5, 10);
+    //nokia_lcd_write_string("Nice!", 3);
+	//nokia_lcd_write_char(0x7F, 1);
+	//nokia_lcd_set_cursor(10, 10);
+	//nokia_lcd_write_char(0x80, 1);
     nokia_lcd_render();
-	//while (1)
-	//{
-	//}
+	while (1)
+	{	
+		while (!TimerFlag) {}
+		if (~PINB & 0x01) {
+			if (XPos > 8) {
+				XPos -= 10;
+			}
+		}
+		if (~PINB & 0x02) {
+			if (XPos < 68) {	
+				XPos += 10;
+			}
+		}
+		if (~PINB & 0x04 && !shootLaser) {
+			shootLaser = 1;
+		}
+		TimerFlag = 0;
+		nokia_lcd_clear();
+		nokia_lcd_set_cursor(XPos, YPos);
+		nokia_lcd_write_char(0x7F, 1);
+		nokia_lcd_set_cursor(XPos+5, YPos);
+		nokia_lcd_write_char(0x80, 1);
+		if (shootLaser == 1) {
+			nokia_lcd_set_cursor(XPos+2, LaserYPosStart - LaserYPos);
+			nokia_lcd_write_char(0x81, 1);
+			LaserYPos += 3;
+		}
+		nokia_lcd_render();
+		if (LaserYPosStart - LaserYPos < 5) {
+			shootLaser = 0;
+			LaserYPos = 0;
+		}
+	}
 	return 0;
 }
 
